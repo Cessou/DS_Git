16c35c802cab73763fb7c22749e296a6aaa3a5f3
Cessou <titouan.cessou@gmail.com>
2017-10-12 14:54:36 +0200

    Affectation IP, port et decodage du message

--- client_UDP.py
+++ client_UDP.py
@@ -1,21 +1,33 @@
 # -*- coding: utf-8 -*-
 
-import socket
+import socket #on importe la bibliotheque socket
 
+ip = "192.168.0.202" #l'adresse ip associé à la variable ip
+port = 5005 #le port associé à port
+msg = "cinema" #le message envoyé : cinema
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
-sock.settimeout(1.0)
+sock.settimeout(1.0) #le delai d'expiration
 
-sock.connect(???)
-sock.send(???)
+sock.connect((ip, port)) #la commande sock.connect importé de socket a besoin de
+#l'ip et du port qu'on a mit enregistrer dans les variables ip et port
+sock.send(msg) #sock.send est importé de socket, il envoie le contenue de la variable msg
 
+trameReponse, addr = sock.recvfrom(1024) #on recupere la reponse ans une variable trameReponse, et
+# l'adresse du server dans addr, on peut recevoir un maximun de 1024octet
 
-trameReponse, addr = sock.recvfrom(1024)
+print "Réception de la trame de réponse", trameReponse.encode("hex") #on affiche le contenue de la trame
+#de reponse qui est encode en hexadecimal
 
-print "Réception de la trame de réponse", trameReponse.encode("hex")
+b0= ord(trameReponse[3])<<24
+b1= ord(trameReponse[2])<<16
+b2= ord(trameReponse[1])<<8
+b3= ord(trameReponse[0])
+# il s'agit d'un decalage de bit
 
-code = ???
+code = b0|b1|b2|b3
+# on assemble les octets, qu'on enregistre dans la variable code
 
 print code
-
+#on affiche le code
